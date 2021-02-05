3987c824e67271801d050f41b050a689462769c4
wlofgren <30736489+wlofgren@users.noreply.github.com>
2021-02-04 22:29:59 -0800

    Update custom.js
    add remove player function

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,10 @@
 		$('.add-new-player').click(function() {
 			$('.clone .single-player').clone(true).appendTo('.clues');
 		});
+	    
+	    	$('.remove-player').click(function() {
+			$(this).parent().parent().remove();
+		});
 
 		$('li a').click(function() {
 			$(this).toggleClass('text-decoration-line-through btn-danger');
